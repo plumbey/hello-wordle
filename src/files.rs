4ee40a4f50efcdceec1b0b873a05d850d2aebd03
plumbey <148833230+plumbey@users.noreply.github.com>
2025-07-18 22:29:52 -0700

    (update) use static word string instead of file

--- src/files.rs
+++ src/files.rs
@@ -1,30 +1,17 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::io::{Error, ErrorKind};
 use rand::random_range;
 
-pub fn get_random_wordle_word(path : &str) -> Result<String, Error> {
-    let line_count = file_line_count(path)?;
-
-    let file = File::open(path)?;
-
-    let reader = BufReader::new(file);
+pub fn get_random_wordle_word(words : &str) -> Result<String, Error> {
+    let line_count = words.lines().count();
 
     let rand_line_num = random_range(0..line_count);
 
-    match reader.lines().nth(rand_line_num) {
-        Some(word) => return word,
+    match words.lines().nth(rand_line_num) {
+        Some(word) => return Ok(word.to_string()),
         None => return Err(
             Error::new(
                 ErrorKind::Other,
                 "Error, could not find word in file {path}!"
                 )),
     }
-}
-
-fn file_line_count(path : &str) -> Result<usize, Error> {
-    let file = File::open(path)?;
-
-    let reader = BufReader::new(file);
-
-    Ok(reader.lines().count())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod words;
 mod files;
 
 const WORD_LENGTH : usize = 5;
-const WORDLE_WORDS_FILE : &str = "./assets/valid_wordle_words.txt";
+const WORDLE_WORDS : &'static str = 
+    include_str!("assets/valid_wordle_words.txt");
 
 fn main() {
-    
-    let secret_word = files::get_random_wordle_word(WORDLE_WORDS_FILE);
+    let secret_word = files::get_random_wordle_word(WORDLE_WORDS);
     match secret_word {
         Ok(word) => println!("{word}"),
         Err(_) => (),
