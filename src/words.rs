9381c83efe7ef57deabae7af1573b549a5e9e25c
plumbey <148833230+plumbey@users.noreply.github.com>
2025-07-19 00:31:58 -0700

    (style) move pub words.rs functions to top

--- src/words.rs
+++ src/words.rs
@@ -6,11 +6,27 @@ enum LetterColor {
     Green,
 }
 
+
+pub fn grade_word(guess : &mut String, secret : &str) -> bool {
+
+    for (i, c) in guess.as_str().chars().enumerate() {
+        print!("{}",color_escape_sequence(letter_color(i, c, secret)));
+        print!("{c}");
+    }
+
+    // reset colors
+    print!("{}",color_escape_sequence(LetterColor::Black));
+    println!();
+
+    guess == secret
+}
+
 pub fn get_valid_word(desired_len : usize) -> String {
     loop {
         let mut s = String::new();
 
-        if io::stdin().read_line(&mut s).is_err() { println!("Error reading word, please try again.") }
+        if io::stdin().read_line(&mut s).is_err() 
+        { println!("Error reading word, please try again.") }
 
         if s.trim().len() == desired_len {
             return s.trim().to_string();
@@ -34,18 +50,4 @@ fn color_escape_sequence(color : LetterColor) -> &'static str {
         LetterColor::Yellow => "\x1b[43m",
         LetterColor::Green => "\x1b[42m",
     }
-}
-
-pub fn grade_word(guess : &mut String, secret : &str) -> bool {
-
-    for (i, c) in guess.as_str().chars().enumerate() {
-        print!("{}",color_escape_sequence(letter_color(i, c, secret)));
-        print!("{c}");
-    }
-
-    // reset colors
-    print!("{}",color_escape_sequence(LetterColor::Black));
-    println!();
-
-    guess == secret
 }
